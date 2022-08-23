1e07605f8485319cd510817ce52816fafe9bde2f
OMISTAJA <Inka.Ristola@metropolia.fi>
2022-08-23 14:27:43 +0300

    pliis

--- tehtävä 2.5.py
+++ tehtävä 2.5.py
@@ -1,7 +1,7 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä massan keskiaikaisten mittojen mukaan leivisköinä, nauloina
 #ja luoteina. Ohjelma muuntaa syötteen täysiksi kilogrammoiksi ja grammoiksi sekä ilmoittaa tuloksen käyttäjälle.
 import math
-math.floor(0)
+math.floor(0.0)
 
 leivis = float(input('Anna leiviskät: '))
 naulat = float(input('Anna naulat: '))
@@ -13,11 +13,9 @@ gluodit = 13.3 * luodit
 
 massa = gnaulat + gleivis + gluodit
 kilo = massa / 1000
-gyli = massa - 1000
+gyli = massa - kilo
 
-if gyli<0:
-    gyli = 0
 
-print(f'Massa nykymittojen mukaan: \n {kilo: 7.2f} kilogrammaa ja{gyli:7.2f} grammaa')
+print(f'Massa ({massa})nykymittojen mukaan: \n {kilo: 7.0f} kilogrammaa ja{gyli:7.2f} grammaa')
 
 
