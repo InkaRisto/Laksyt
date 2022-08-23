faf79170723f0340fe856724fb41f5c4e2a43eb6
OMISTAJA <Inka.Ristola@metropolia.fi>
2022-08-23 16:33:47 +0300

    Tehty

--- tehtävä 2.6.py
+++ tehtävä 2.6.py
@@ -5,11 +5,25 @@
 
 import random
 
-z = random.randint(0, 9)
-x =
+kolmikko = []
+neljä = []
 
-for luku in range(3):
-         print(luku)
+for i in range (3):
+    kolmikko.append(random.randint(0, 9))
+
+for i in range (4):
+    neljä.append(random.randint(1, 6))
+
+print(kolmikko)
+print(neljä)
+
+
+
+#En tykkää outputin ulkomuodosta, mutta en saanut listoja toimimaan nätisti Pythonilla. Dartilla oli toString...
+
+
+
+
 
 
 
