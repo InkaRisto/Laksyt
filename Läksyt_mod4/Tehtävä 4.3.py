c6f71d817dfb42b1e4008e9e1812a4c896cb7d00
OMISTAJA <Inka.Ristola@metropolia.fi>
2022-09-01 15:31:19 +0300

    Mod4.6

--- Läksyt_mod4/Tehtävä 4.3.py
+++ Läksyt_mod4/Tehtävä 4.3.py
@@ -2,21 +2,21 @@
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 
-put = (input('Syötä luku (Tyhjä lopettaa): '))
-min = 1
-max = 1
+put = input('Syötä luku (Tyhjä lopettaa): ')
+minV = float(put)
+maxV = minV
 
 while put != '':
     numb = float(put)
-    if numb < min:
-       min = numb
-    elif numb > max:
-        max = numb
+    if numb < minV:
+        minV = numb
+    elif numb > maxV:
+        maxV = numb
     else:
         put = input('Syötä uusi luku: ')
 
 print('Ohjelma on päättynyt.')
 
-if min != 1 and max != 1:
-    print(f'Pienin syöttämäsi luku on {min} ja suurin on {max}')
+if minV != 1 and maxV != 1:
+    print(f'Pienin syöttämäsi luku on {minV} ja suurin on {maxV}')
 
---
+++ Läksyt_mod4/Tehtävä 4.6.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+N = int(input('Montako pistettä kokeillaan? '))
+points = 0
+
+while points < N:
+    points = points + 1
+    xy = random.
+xy =
+lista = []
+if xy = x^2+y^2<1:
+    lista.append(xy)
+
+print(len(lista))
+
