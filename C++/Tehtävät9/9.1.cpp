952b24d3521acb231faf88288987797c7b180cda
InkaRisto <111732537+InkaRisto@users.noreply.github.com>
2023-12-07 09:35:35 +0200

    tuntimuokkaukset

--- C++/Tehtävät9/9.1.cpp
+++ C++/Tehtävät9/9.1.cpp
@@ -3,25 +3,41 @@
 using namespace std;
 
 int main() {
-	int alloSize = 1000;
+	int alloSize = 100000;
 	int* alloed = nullptr;
 
-	try {
-		while (true) {
-			alloed = new int[alloSize];
-			cout << "Currently allocated: " << alloSize << endl;
-			alloSize = +1000;
+	//try {
+	//	while (true) {
+	//		alloed = new int[alloSize];
+	//		cout << "Currently allocated: " << alloSize * sizeof(int) << endl;
+	//		alloSize += 100000;
 
+	//	}
+
+	//}
+	//catch (bad_alloc& err) {
+	//	cout << "Failed to allocate memory. Error: " << err.what() << endl;
+	//	cout << "Successfully allocated: " << alloSize;
+
+	//}
+
+	while (true) {
+		alloed = static_cast<int*>(malloc(alloSize));
+		cout << "Currently allocated: " << alloSize << endl;
+		if (alloSize > 0) {
+			alloSize += 100000;
 		}
-
-	}
-	catch (bad_alloc& err) {
-		cout << "Failed to allocate memory. Error: " << err.what() << endl;
-		cout << "Successfully allocated: " << alloSize;
-
+		else {
+			break;
+		}
 	}
 
+	cout << "Failed to allocate memory." << endl;
+	cout << "Successfully allocated: " << alloSize;
+	free(alloed);
+	
 	delete[] alloed;
+	
 	return 0;
 
 }
